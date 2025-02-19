c4af903161bbee535291167733ed6c514afc3bea
apal2702 <apal@twilio.com>
2025-02-19 12:26:50 +0530

    update app
    update app

--- app/account_info.py
+++ app/account_info.py
@@ -5,34 +5,28 @@ def get_account_details(account_sid, country = None, mcc = None):
     acc_info_obj = AccountInfo(st, account_sid, country, mcc)
     acc_info_obj.get_account_info()
 
-    #return account_info
+    #return account_inf
+def account_info_app():
+    #st.set_page_config(layout="wide")
 
+    # Streamlit app
+    st.title("FG/ SPP Account Analysis")
 
-def chatbot_response(user_input):
-    # Simple example bot logic
-    return f"Bot: You said '{user_input}'"
+    # Text input
+    user_input = st.text_input("Enter Account SID", max_chars = 40)
+    # Additional inputs
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        country_input = st.text_input("Enter Country (optional)", key="country")
+    with col2:
+        mcc_input = st.text_input("Enter MCC (optional)", key="mcc") 
+    with col3:
+        mnc_input = st.text_input("Enter MNC (optional)", key="mnc")
 
-st.set_page_config(layout="wide")
-
-# Streamlit app
-st.title("FG/ SPP Account Analysis")
-
-# Text input
-user_input = st.text_input("Enter Account SID", max_chars = 40)
-# Additional inputs
-col1, col2, col3 = st.columns(3)
-with col1:
-    country_input = st.text_input("Enter Country (optional)", key="country")
-with col2:
-    mcc_input = st.text_input("Enter MCC (optional)", key="mcc") 
-with col3:
-    mnc_input = st.text_input("Enter MNC (optional)", key="mnc")
-
-
-# Button
-if st.button("Send"):
-    if user_input:
-        response = get_account_details(user_input)
-        st.write(response)
-    else:
-        st.write("Please enter a message.")
+    # Button
+    if st.button("Send"):
+        if user_input:
+            response = get_account_details(user_input)
+            st.write(response)
+        else:
+            st.write("Please enter a message.")
--- main_app.py
+++ main_app.py
@@ -4,12 +4,13 @@ import streamlit as st
 # Import page functions at the top
 from app.fg_bot_app import fg_bot_app
 from app.llm_test import llm_test
+from app.account_info import account_info_app
 # Create a title for the app
 st.title("DSG Auto Assist")
 
 # Create a sidebar with navigation links
 st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Select a page",["Home","FG HELP Bot","LLM Testing"] )
+page = st.sidebar.selectbox("Select a page",["Home","FG HELP Bot","LLM Testing","Account Info"] )
 
 # Call the appropriate page function based on selection
 
@@ -17,3 +18,5 @@ if page == "FG HELP Bot":
     fg_bot_app()
 if page == "LLM Testing":
     llm_test()
+if page == "Account Info":
+    account_info_app()
